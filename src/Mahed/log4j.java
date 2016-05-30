f97586627b00e59c925f18d01621b1bd7915b143
Mahed Ali <Mahed Ali@172.16.105.229>
2016-05-30 17:19:43 +0500

    hn hn main maujram hun

---
+++ src/Mahed/log4j.java
@@ -0,0 +1,23 @@
+package Mahed;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class log4j {
+	private static Logger logger = Logger.getLogger(log4j.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//DOMConfigurator.configure("log4j.xml");
+				PropertyConfigurator.configure("log4j.properties");
+				//BasicConfigurator.configure();
+				try{
+					int i = 0;
+					i = i / 0;
+				}catch(Exception e){
+					logger.error(e.getMessage());
+				}
+	}
+
+}
